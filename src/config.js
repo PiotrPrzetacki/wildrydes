05c95c45f36ba321cecd1f396985335f6538459e
PiotrPrzetacki <91681245+PiotrPrzetacki@users.noreply.github.com>
2024-01-08 09:58:27 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 // Add your API invoke URL below, configured in Module 3. 
 
-module.exports = {
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_CflORqEQa', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6pkdvankdjds2mfdh4tqj3vv7i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
+    },
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
-}
+};
