fcdc1486bfacd979e7d10d3864ca87d7893f0d96
PiotrPrzetacki <91681245+PiotrPrzetacki@users.noreply.github.com>
2024-01-08 10:41:31 +0100

    Update config.js in public

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_CflORqEQa', // e.g. eu-east-2_uXboG5pAb
+        userPoolId: 'eu-west-1_CflORqEQa', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6pkdvankdjds2mfdh4tqj3vv7i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://uqx0xo8tt3.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
