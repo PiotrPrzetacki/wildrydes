7d4fe7714d619fdbd21db62cb0a965bfe12af3a3
PiotrPrzetacki <91681245+PiotrPrzetacki@users.noreply.github.com>
2024-01-08 10:21:58 +0100

    Update config.js invokeUrl

--- src/config.js
+++ src/config.js
@@ -7,6 +7,6 @@ window._config = {
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://uqx0xo8tt3.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
